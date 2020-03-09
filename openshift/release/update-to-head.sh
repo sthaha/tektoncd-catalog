6c332d8e60cee7471cc44605e34ee14d16811228
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-03-09 15:17:26 +1000

    Be explicit about what gets pushed where

--- openshift/release/update-to-head.sh
+++ openshift/release/update-to-head.sh
@@ -8,22 +8,22 @@ REPO_NAME=`basename $(git rev-parse --show-toplevel)`
 
 # Reset release-next to upstream/master.
 git fetch upstream master
-git checkout upstream/master -B release-next
+git checkout upstream/master --no-track -B release-next
 
 # Update openshift's master and take all needed files from there.
 git fetch openshift master
-git checkout openshift/master openshift OWNERS_ALIASES OWNERS 
-git add openshift OWNERS_ALIASES OWNERS 
+git checkout openshift/master openshift OWNERS_ALIASES OWNERS
+git add openshift OWNERS_ALIASES OWNERS
 git commit -m ":open_file_folder: Update openshift specific files."
 
-git push -f openshift release-next
+git push -f openshift HEAD:release-next
 
 # Trigger CI
-git checkout release-next -B release-next-ci
+git checkout release-next --no-track -B release-next-ci
 date > ci
 git add ci
 git commit -m ":robot: Triggering CI on branch 'release-next' after synching to upstream/master"
-git push -f openshift release-next-ci
+git push -f openshift HEAD:release-next-ci
 
 if hash hub 2>/dev/null; then
    hub pull-request --no-edit -l "kind/sync-fork-to-upstream" -b openshift/${REPO_NAME}:release-next -h openshift/${REPO_NAME}:release-next-ci
